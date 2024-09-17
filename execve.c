70cbff2b4af587df9d242df0807c1af96a911e92
yu-432 <o.shimayu891@gmail.com>
2024-09-17 12:24:27 +0900

    :+1: execveコンパイルオプション追加

--- execve.c
+++ execve.c
@@ -8,13 +8,19 @@
 
 #include "libft/libft.h"
 
-// cc execve.c libft/ft_split.c libft/ft_memcpy.c -lreadline -lhistory
+//execve関数(関数へのフルパス、起動する関数への引数(オプション)、環境変数へのポインタ)
+//第一引数にはフルパスを渡す必要がある(今試した所)のでどこかでPATHに対してコマンドを付け加えた
+//フルパスを作成し、渡す必要がある
+//execve関数が正常に動作した場合このプロセスは参照先のコマンドのプロセスに完全に置き換わるため、
+// どこでmallocした文字列をfreeするかが問題
+// 	->構造体かなにかにアドレスを保持しておく？
+// access関数を用いることでファイルの確認をすることが可能だが、する必要性が特にないため書いていない
 
-
+// cc execve.c libft/ft_split.c libft/ft_memcpy.c libft/ft_strjoin.c libft/ft_strlen.c -lreadline -lhistory
 
 int interpret(char *line, char **argv)
 {
-	// extern char **environ;
+	extern char **environ;
 	char **path;
 	char *joined;
 	char *adsl;
@@ -29,20 +35,18 @@ int interpret(char *line, char **argv)
 	}
 	if (pid == 0)
 	{
-		// printf("getenv %s\n", getenv("PATH"));
 		path = ft_split(getenv("PATH"), ':');
-		// for(int i = 0; path[i] != NULL; i++)
-		// 	printf("path = %s\n", path[i]);
 		printf("child active\n");
 		for(int i = 0; path[i] != NULL; i++)
 		{
 			adsl = ft_strjoin("/", line);
 			joined = ft_strjoin(path[i], adsl);
 			free(adsl);
-			execve(joined, argv, path);
+			execve(joined, argv, environ);
+			printf("invalid path\n");
 			free(joined);
 		}
-		printf("test\n");
+		printf("command not found\n");
 		exit(EXIT_SUCCESS);
 	}
 	if (pid > 0)
