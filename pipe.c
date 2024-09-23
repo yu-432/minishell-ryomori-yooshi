148c5d711a05b039d53292a1526a57a2d7a95768
Mori0113 <ruhakaforest.kaki.88@gmail.com>
2024-09-23 15:20:37 +0900

    :+1: add : pipe 1

--- pipe.c
+++ pipe.c
@@ -8,21 +8,32 @@
 int main ()
 {
     pid_t   pid;
+    int fds[2];
 
+    fds[0] = 100;
+    fds[1] = 200;
+
+    pipe(fds); 
     pid = fork();
-
     if (pid > 0)
     {
         int status;
 
-        printf("before wait\n");
+        // printf("before wait\n");
         waitpid(pid, &status, 0);
+        
+        close(fds[0]);
+        // printf("fds[0]%d,fds[1]%d\n", fds[0], fds[1]);
+        
+
         printf("parent\n");
         exit(0);
     }
     if (pid == 0)
     {
         printf("I am good\n");
+        close(fds[1]);
+        // printf("fds[0]%d,fds[1]%d\n", fds[0], fds[1]);
         exit(0);
     }
     else
