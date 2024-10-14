bd4918b3d59251443e51ca22b50bb07cb62a4e09
yu-432 <o.shimayu891@gmail.com>
2024-10-14 12:08:11 +0900

    :+1: unclosed quote unknown kind検出

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -1,19 +1,22 @@
 #include "../../header/lexer.h"
 #include "../../header/standard.h"
 
+
+
 bool lexer(t_condition *condition, char *line)
 {
 	t_token *tokenized;
+	int count = 1;
 
 	tokenized = tokenizer(line);
+	find_syntax_error(condition, tokenized);
 	expand_token(condition, tokenized);
 
 	while(tokenized)
 	{
-		printf("token:%s ", tokenized->token);
-		printf("kind:%d\n", tokenized->kind);
+		printf("token[%u] kind = %u : %s\n", count, tokenized->kind, tokenized->token);
+		count++;
 		tokenized = tokenized->next;
-		printf("----------next----------\n");
 	}
 	(void)condition;
 	return (true);
