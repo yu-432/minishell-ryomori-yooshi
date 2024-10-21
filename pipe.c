60bac88390343334b4d1bc6945ad62e730f9e97f
Ryosuke Mori <ryomori@c2r5s1.42tokyo.jp>
2024-10-21 18:29:18 +0900

    :+1:pipeにredirectの処理を加えた

--- pipe.c
+++ pipe.c
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-
+#include <fcntl.h>
 
 //==================================================================================
 //構造体
@@ -240,20 +240,33 @@ char **decision_args(t_token *token)
 
 	while (tmp_token && tmp_token->kind != TOKEN_PIPE)
 	{
-		list_elem_num++;
-		// printf("token: %s\n",tmp_token->token);
+		if (tmp_token->kind != TOKEN_REDIRECT_IN &&
+			tmp_token->kind != TOKEN_REDIRECT_OUT &&
+			tmp_token->kind != TOKEN_REDIRECT_APPEND)
+		{
+			list_elem_num++;
+		}
 		tmp_token = tmp_token->next;
 	}
 
 	char	**args = (char**)malloc(sizeof(char*) * (list_elem_num + 1));
-
+	if (!args)
+	{
+		perror("malloc");
+		exit(1);
+	}
+	
 	tmp_token = token;
 	i = 0;
 	while(tmp_token && tmp_token->kind != TOKEN_PIPE)
 	{
-		args[i] = strdup(tmp_token->token);
+		if (tmp_token->kind != TOKEN_REDIRECT_IN &&
+		tmp_token->kind != TOKEN_REDIRECT_OUT &&
+		tmp_token->kind != TOKEN_REDIRECT_APPEND)
+		{
+			args[i++] = strdup(tmp_token->token);
+		}
 		tmp_token = tmp_token->next;
-		i++;
 	}
 	args[i] = NULL;
 	// int num = 0;
@@ -266,6 +279,91 @@ char **decision_args(t_token *token)
 	return(args);
 }
 
+//==================================================================================
+//==================================================================================
+//redirect
+
+int	redirections(t_token **token)
+{
+	int	fd;
+
+	fd = -1;
+	while (*token)
+	{
+		if ((*token)->kind == TOKEN_REDIRECT_IN)
+		{
+			*token = (*token)->next;
+			if (!*token)
+			{
+				fprintf(stderr, "Error: Token redirect < IN\n");
+				return (-1);
+			}
+			fd = open((*token)->token, O_RDONLY);
+			if (fd == -1)
+			{
+				perror("open");
+				return (-1);
+			}
+			if (dup2(fd, STDIN_FILENO) == -1)
+			{
+				close(fd);
+				perror("dup2");
+				return (-1);
+			}
+			close (fd);
+		}
+		else if ((*token)->kind == TOKEN_REDIRECT_OUT)
+		{
+			*token = (*token)->next;
+			if (!*token)
+			{
+				fprintf(stderr, "Error: Token resirect > OUT\n");
+				return (-1);
+			}
+			fd = open((*token)->token, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+			if (fd == -1)
+			{
+				perror("open");
+				return (-1);
+			}
+			if(dup2(fd, STDOUT_FILENO) == -1)
+			{
+				close(fd);
+				perror("dup2");
+				return (-1);
+			}
+			close(fd);
+		}
+		else if ((*token)->kind == TOKEN_REDIRECT_APPEND)
+		{
+			*token = (*token)->next;
+			if (!*token)
+			{
+				fprintf(stderr, "Error: Token redirect >> append\n");
+				return (-1);
+			}
+			fd = open((*token)->token, O_WRONLY | O_CREAT | O_APPEND, 0644);
+			if (fd == -1)
+			{
+				perror("open");
+				return(-1);
+			}
+			if (dup2(fd, STDOUT_FILENO) == -1)
+			{
+				close(fd);
+				perror("dup2");
+				return (-1);
+			}
+			close(fd);
+		}
+		else
+		{
+			break;
+		}
+		*token = (*token)->next;
+	}
+	return (0);
+}
 
 //==================================================================================
 //==================================================================================
@@ -289,11 +387,13 @@ void	com_token_pipe(t_token *token, int num_com)
 		{
 			if (i < num_com - 1 && pipe(fds) == -1)
 			{
-				exit;
+				perror("pipe");
+				exit(1);
 			}
 			pid = fork();
 			if (pid == -1)
 			{
+				perror("fork");
 				exit(1);
 			}
 			if (pid == 0)
@@ -303,18 +403,21 @@ void	com_token_pipe(t_token *token, int num_com)
 				{
 					fd_output_child(fds);
 				}
-				// printf("tmp->token %s\n", tmp_token->token);
+				
+				if (redirections(&tmp_token) == -1)
+				{
+					exit(1);
+				}
+				
 
+				// printf("tmp->token %s\n", tmp_token->token);
 				args = decision_args(tmp_token);
 				fprintf(stderr, "find_command :%s\n", find_command(args[0]));
 				fprintf(stderr, "args[0] = %s\n args[1] = %s\n", args[0], args[1]);
 				execve(find_command(args[0]), args, environ);
-				perror("execve");  // エラーが発生した場合のメッセージを表示
-					exit(1);
-
-
+				perror("execve");// エラーが発生した場合のメッセージを表示
 				free(args);
-				exit(0);
+					exit(1);
 			}
 			else if (pid > 0)
 				keep_fd_update(fds, &keep_fd, i, num_com);
@@ -338,9 +441,8 @@ void	com_token_pipe(t_token *token, int num_com)
 
 
 
-
-
-
+//---------------------------------------------------------------------------------------------
+//creat_token
 
 t_token *create_token(char *str, t_token_kind kind) {
     t_token *new_token = (t_token *)malloc(sizeof(t_token));
@@ -355,32 +457,80 @@ t_token *create_token(char *str, t_token_kind kind) {
 }
 
 
+//============================================================================================================
+//cat < input.txt | grep 'a' | sort | uniq | tee > log.txt
+//リダイレクトを含んだpipe
+
 int main()
 {
-    t_token *token1 = create_token("ls", TOKEN_WORD);
-    t_token *token2 = create_token("-l", TOKEN_WORD);
+	t_token *token1 = create_token("cat", TOKEN_WORD);
+	t_token *token2 = create_token("<", TOKEN_REDIRECT_IN);
+	t_token *token3 = create_token("input.txt", TOKEN_WORD);
+	t_token *token4 = create_token("|", TOKEN_PIPE);
+	t_token *token5 = create_token ("grep", TOKEN_WORD);
+	t_token *token6 = create_token ("a", TOKEN_WORD);
+	t_token *token7 = create_token ("|", TOKEN_PIPE);
+	t_token *token8 = create_token ("sort", TOKEN_WORD);
+	t_token *token9 = create_token ("|", TOKEN_PIPE);
+	t_token *token10 = create_token ("uniq", TOKEN_WORD);
+	t_token *token11 = create_token ("|", TOKEN_PIPE);
+	t_token *token12 = create_token ("tee", TOKEN_WORD);
+	t_token *token13 = create_token(">", TOKEN_REDIRECT_OUT);
+	t_token *token14 = create_token("log.txt", TOKEN_WORD);
 
-    t_token *token3 = create_token("|", TOKEN_PIPE);
-    
-	t_token *token4 = create_token("grep", TOKEN_WORD);
-    t_token *token5 = create_token(".c", TOKEN_WORD);
-    
-	// t_token *token6 = create_token("|", TOKEN_PIPE);
-    
-	// t_token *token7 = create_token("touch", TOKEN_WORD);
-	// t_token *token8 = create_token("-l", TOKEN_WORD);
 
-    // トークンをリンク
-    token1->next = token2;
-    token2->next = token3;
-    token3->next = token4;
-    token4->next = token5;
-    // token5->next = token7;
-    // token6->next = token7;
-	// token7->next = token8;
 
-    // パイプを用いてコマンド実行
-    com_token_pipe(token1, 2);
 
-    return (0);
+	token1->next = token2;
+	token2->next = token3;
+	token3->next = token4;
+	token4->next = token5;
+	token5->next = token6;
+	token6->next = token7;
+	token7->next = token8;
+	token8->next = token9;
+	token9->next = token10;
+	token10->next = token11;
+	token11->next = token12;
+	token12->next = token13;
+	token13->next = token14;
+
+	com_token_pipe(token1, 5);
+
+	return(0);
 }
+
+
+//============================================================================================================
+//pipeのみtest code
+
+
+// int main()
+// {
+//     t_token *token1 = create_token("ls", TOKEN_WORD);
+//     t_token *token2 = create_token("-l", TOKEN_WORD);
+
+//     t_token *token3 = create_token("|", TOKEN_PIPE);
+    
+// 	t_token *token4 = create_token("grep", TOKEN_WORD);
+//     t_token *token5 = create_token(".c", TOKEN_WORD);
+    
+// 	// t_token *token6 = create_token("|", TOKEN_PIPE);
+    
+// 	// t_token *token7 = create_token("touch", TOKEN_WORD);
+// 	// t_token *token8 = create_token("-l", TOKEN_WORD);
+
+//     // トークンをリンク
+//     token1->next = token2;
+//     token2->next = token3;
+//     token3->next = token4;
+//     token4->next = token5;
+//     // token5->next = token7;
+//     // token6->next = token7;
+// 	// token7->next = token8;
+
+//     // パイプを用いてコマンド実行
+//     com_token_pipe(token1, 2);
+
+//     return (0);
+// }
