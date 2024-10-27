26a2f5bb8c45733885390dca7570a9f182030844
yooshima <o.shimayu891@gmail.com>
2024-10-27 15:02:03 +0900

    :+1: それっぽい関数を実装したがfreeされたか確認する必要がある

---
+++ src/builtin_func/builtin_unset.c
@@ -0,0 +1,32 @@
+#include "../../header/standard.h"
+#include "../../libft/libft.h"
+#include "../../header/condition.h"
+#include "../../header/environ.h"
+
+void free_t_item(t_item **item)
+{
+	free((*item)->key);
+	free((*item)->value);
+	free(*item);
+}
+
+void builtin_unset(t_condition *condition, char **argv)
+{
+	int i;
+	t_item *delete_key;
+
+	i = 1;
+	while(argv[i])
+	{
+		delete_key = search_dup_key(condition, argv[i]);
+		if (!delete_key)
+		{
+			i++;
+			continue;
+		}
+		delete_key->prev->next = delete_key->next;
+		delete_key->next->prev = delete_key->prev;
+		free_t_item(&delete_key);
+		i++;
+	}
+}
