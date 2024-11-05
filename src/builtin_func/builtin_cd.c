f7a8d01e5db6a3f4633933db68481de3810ae6db
Ryosuke Mori <ryomori@c2r5s1.42tokyo.jp>
2024-11-05 15:03:16 +0900

    :+1:cdの改善

--- src/builtin_func/builtin_cd.c
+++ src/builtin_func/builtin_cd.c
@@ -100,7 +100,10 @@ static int move_path(int option, t_condition cond)
 int builtin_cd(t_condition *cond, char **args)
 {
 	int	judge;
+	char newcwd[PATH_MAX];
+	char *pwd;
 
+	pwd = lst_getenv(cond, "PWD");
 	if(!args[1] || !ft_strncmp(args[1], "~", 2))
 		judge = (move_path(MOVE_TO_HOME, *cond));
 	if (ft_strncmp(args[1], "-", 2) == 0)
@@ -112,5 +115,67 @@ int builtin_cd(t_condition *cond, char **args)
 	}
 	if(judge < 0)
 		perror("cd");
+	newcwd = recreate_cwd(pwd, args[1]);//作成中
+	set_newcwd(cond, newcwd);//作成中
 	return (0);//success or fail
+}
+
+//=============================================================================
+//==========================        lst_getenv       ==========================
+//=============================================================================
+
+char *lst_getenv(t_item *item, char *key)
+{
+	char *value;
+
+	while(item)
+	{
+		if(ft_strncmp(item->key, key, ft_strlen(key) + 1) == 0)
+		{
+			value = item->value;
+			return (value);
+		}
+		item = item->next;
+	}
+	return (NULL);
+}
+
+//=============================================================================
+//==========================        recreate_cwd       ==========================
+//=============================================================================
+
+char *recreate_cwd(char *pwd, char *args)
+{
+	char newcwd[PATH_MAX];
+	char *tmp;
+
+	if(*args == '/' || pwd == NULL)
+		ft_strlcpy(newcwd, "/", PATH_MAX);
+	else
+		ft_strlcpy(newcwd, pwd, PATH_MAX);
+
+	while(*args)
+	{
+		if(*args == '/')
+			args++;
+		else if(ft_strncmp(args, ".", 3) == 0)
+	}
+	
+	return (newcwd);
+}
+
+bool character_elimination(char ***newcwd, char *args, char *element)
+{
+	size_t len;
+
+	len = ft_strlen(element);
+	if (ft_strncmp(args, element, len) == 0)
+	{
+		if(args[len] == '\0' || args[len] == '/')
+		{
+			*newcwd = args + len;
+			return
+		}
+	}
+	return (false);
 }
