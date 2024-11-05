188345250133f93cfa8c356d7379856722371e1f
yu-432 <o.shimayu891@gmail.com>
2024-11-05 18:47:53 +0900

    :+1: echo "<"の実装

--- src/execution/node.c
+++ src/execution/node.c
@@ -5,7 +5,31 @@
 #include "../../header/execution.h"
 #include "../../libft/libft.h"
 
-t_node *new_node(char **argv)
+int set_redirect(t_node *node, t_token **token_list)
+{
+	bool is_success;
+	t_token_kind kind;
+
+	is_success = true;
+	kind = (*token_list)->kind;
+	if(kind == TOKEN_REDIRECT_IN)
+		is_success = redirect_in(node, *token_list);
+	else if(kind == TOKEN_REDIRECT_OUT)
+		is_success = redirect_out(node, *token_list);
+	else if(kind == TOKEN_REDIRECT_APPEND)
+		is_success = redirect_append(node, *token_list);
+	else if(kind == TOKEN_REDIRECT_HEREDOC)
+		is_success = redirect_heredoc(node, *token_list);
+	if(!is_success)
+		return(1);
+	*token_list = (*token_list)->next;
+	if (!(*token_list))
+		return (0);
+	*token_list = (*token_list)->next;
+	return (0);
+}
+
+t_node *new_node(void)
 {
 	t_node *node;
 
@@ -13,17 +37,22 @@ t_node *new_node(char **argv)
 	if (node == NULL)
 		return (NULL);
 	node->next = NULL;
-	node->argv = argv;
 	node->fd_in = -2;
 	node->fd_out = -2;
-	if (is_pipe(argv[0]))
-		node->kind = NODE_PIPE;
-	else
-		node->kind = NODE_CMD;
 	return (node);
 }
 
-int count_tokens_until_pipe(t_token *token_list)
+bool is_redirect(t_token_kind kind)
+{
+	if (kind == TOKEN_REDIRECT_IN || \
+		kind == TOKEN_REDIRECT_OUT || \
+		kind == TOKEN_REDIRECT_APPEND || \
+		kind == TOKEN_REDIRECT_HEREDOC)
+		return (true);
+	return (false);
+}
+
+int count_word_until_pipe(t_token *token_list)
 {
 	int count;
 	t_token *current;
@@ -32,58 +61,76 @@ int count_tokens_until_pipe(t_token *token_list)
 	current = token_list;
 	if (current->kind == TOKEN_PIPE)
 		return (1);
-	while(current)
+	while(current && current->kind != TOKEN_PIPE)
 	{
-		if(current->kind ==TOKEN_PIPE)
-			break;
-		count++;
-		current = current->next;
+		if (current->kind == TOKEN_WORD)
+			count++;
+		if (is_redirect(current->kind) && current->next)
+			current = current->next->next;
+		else
+			current = current->next;
 	}
 	return (count);
 }
 
-
-char **make_argv(t_token **token_list)
+char **make_node_argv(t_token **token_list, t_node *current_node)
 {
-	char **argv;
-	int arg_count;
+	int word_count;
 	int i;
 
-	arg_count = count_tokens_until_pipe(*token_list);
-	argv = ft_calloc(arg_count + 1, sizeof(char *));
-	if(!argv)
+	word_count = count_word_until_pipe(*token_list);//TOKEN_WORDの数を数える
+	current_node->argv = ft_calloc(word_count + 1, sizeof(char *));
+	if(!current_node->argv)
 		return(NULL);
 	i = 0;
-	while (i < arg_count)
+	if ((*token_list)->kind == TOKEN_PIPE)
 	{
-		argv[i] = ft_strdup((*token_list)->token);
-		if (!argv[i])
+		current_node->argv[i] = ft_strdup((*token_list)->token);
+		if (!current_node->argv[i])
 			return (NULL);
 		*token_list = (*token_list)->next;
-		i++;
+		return (NULL);
 	}
-	argv[i] = NULL;
-	return (argv);
+
+	while (i < word_count || (*token_list && (*token_list)->kind != TOKEN_PIPE))
+	{
+		if (!is_redirect((*token_list)->kind))
+		{
+			current_node->argv[i] = ft_strdup((*token_list)->token);
+			if (!current_node->argv[i])
+				return (NULL);
+			i++;
+			*token_list = (*token_list)->next;
+		}
+		else if(is_redirect((*token_list)->kind))
+			set_redirect(current_node, token_list);
+	}
+	current_node->argv[i] = NULL;
+	return (NULL);////////////////////////
 }
 
 t_node *make_node(t_token *token_list)
 {
 	t_node head;
 	t_node *current;
-	char **argv;
 
 	head.next = NULL;
 	while(token_list)
 	{
 		current = find_last_node(&head);
-		argv = make_argv(&token_list);
-		if (!argv)
-			return (NULL);
-		current->next = new_node(argv);
+		current->next = new_node();
 		if (!current->next)
 			return (NULL);
+		make_node_argv(&token_list, current->next);//PIPE区切りでargvを作成
+		if (current->next->argv[0] == NULL)
+			return (NULL);
+		if(ft_strncmp(current->next->argv[0], "|\0", 2) == 0)
+			current->next->kind = NODE_PIPE;
+		else
+			current->next->kind = NODE_CMD;
 		current->next->prev = current;
 	}
+	current = head.next;
 	return (head.next);
 }
 
