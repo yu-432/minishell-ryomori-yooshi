c4d012cdc7f432c8f0f97ba3501cc6f10482f05f
yu-432 <o.shimayu891@gmail.com>
2024-11-19 13:32:59 +0900

    :+1: heredoc expand dollar

--- src/execution/heredoc_utils.c
+++ src/execution/heredoc_utils.c
@@ -6,7 +6,7 @@
 /*   By: yooshima <yooshima@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 23:56:13 by yooshima          #+#    #+#             */
-/*   Updated: 2024/11/18 14:51:51 by yooshima         ###   ########.fr       */
+/*   Updated: 2024/11/19 13:28:17 by yooshima         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,6 +42,34 @@ char	*get_line(int fd, int *read_status)
 			return (NULL);
 	}
 	return (result);
+}
+
+bool	expand_heredoc_dollar(t_condition *condition, char **line)
+{
+	int		i;
+	char	*new;
+
+	new = ft_strdup("");
+	if (!new)
+		return (false);
+	i = 0;
+	while ((*line)[i])
+	{
+		if ((*line)[i] == '$')
+		{
+			if (!handle_dollar(condition, *line, &new, &i))
+				return (false);
+		}
+		else
+		{
+			if (!append_char(&new, (*line)[i]))
+				return (false);
+			i++;
+		}
+	}
+	free(*line);
+	*line = new;
+	return (true);
 }
 
 void	heredoc_free_exit(char *line, int fd, int exit_status)
